9662b9d3f40a73bc94bf43ee6ab42c7462274908
Sectioned <amathisont@gmail.com>
2017-03-28 15:51:04 +0100

    Trying

---
+++ KandR/Arrays/Ex1-14.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define IN 0
+#define OUT 1
+
+int main(){
+
+        int c, i;
+
+        int digits[10];
+
+        for(i = 0; i < 10; i++){
+
+                digits[i] = 0;
+
+        }
+
+        while((c = getchar()) != EOF){
+
+
+
+        }
+
+        for(i = 0; i < 10; i++){
+
+                printf("%d", digits[i]);
+
+                for(){
+
+                        
+
+                }
+
+                printf("\n");
+
+        }
+
+}
