f0785bdb1b99fc2f43808011866e47ba49195313
Sectioned <amathisont@gmail.com>
2017-04-06 13:55:45 +0100

    not perfect

--- KandR/Ch1/Final/Ex1-22.c
+++ KandR/Ch1/Final/Ex1-22.c
@@ -1,22 +1,31 @@
 #include <stdio.h>
 
 int c;
-
-int count = scount = 0;
+int count;
+int scount;
 
 int main(){
 
+        count = scount = 0;
+
         while((c = getchar()) != EOF){
 
-                if(count > 10){
+                if(count > 20){
 
                         putchar(c);
 
-                        while((c = getchar()) != ' '){
+                        while((c = getchar()) != ' ' && scount < 5){
                           scount++;
                           putchar(c);
                         }
+                        if(scount >= 9){
+                                putchar('-');
+                        }
                         putchar('\n');
+                        if(scount >= 9){
+                                putchar(c);
+                        }
+                        scount = 0;
                         count = 0;
 
                 }else{
