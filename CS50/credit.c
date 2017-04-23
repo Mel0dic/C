38f8a0b21b3d272fdd1c753777d82f4e16fb7aba
Sectioned <amathisont@gmail.com>
2017-04-23 16:10:13 +0100

    Push

--- CS50/credit.c
+++ CS50/credit.c
@@ -22,15 +22,36 @@ int main(){
 
     ccount = cnum;
 
-    int numb[count];
+    int numb[count-1];
 
-    for(int i = count; i > 0; i--){
+    for(int i = count; i >= 0; i--){
           numb[i] = cnum % 10;
           cnum /= 10;
     }
 
-    for(int i = 1; i <= count; i++){
-          printf("%i = %i\n", i, numb[i]);
+    // for(int i = 0; i <= count; i++){
+    //       printf("%i = %i\n", i, numb[i]);
+    // }
+
+    int twos = count - 1;
+    int test = 0;
+    int trouble = 0;
+
+    while(twos > 0){
+          printf("%d\n", numb[twos]);
+          test += numb[twos] * 2;
+          twos -= 2;
     }
+
+    twos = count;
+
+    while(twos > 0){
+          printf("%d\n", numb[twos]);
+          trouble += numb[twos];
+          twos -= 2;
+    }
+
+    printf("\n%d\n", test);
+    printf("\n%d\n", trouble);
 
 }
