0c42ff330c238e8b0dc2e3177011c06b90b88f0a
Sectioned <amathisont@gmail.com>
2017-04-25 12:21:03 +0100

    changes

--- CS50/pset2/initials.c
+++ CS50/pset2/initials.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
 
-int c;
+int c, count;
 
 int upper(int c);
 
+int name[20];
+
+count = 0;
+
 int main() {
 
-  if((c = getchar()) != ' '){
-    putchar(upper(c));
-  }
-
   while((c = getchar()) != EOF && c != '\n'){
-
+    name[count] = c;
+    count++;
   }
 
-  if(c == ' '){
-    while ((c = getchar()) == ' ') {
-      ;
+  // if(c == ' '){
+  //   while ((c = getchar()) == ' ') {
+  //     ;
+  //   }
+  //   putchar(upper(c));
+  // }else if(c >= 'A' && c <= 'Z'){
+  //   putchar(c);
+  // }
+
+  for(int i = 0; i <= count; i++){
+    if(name[i] >= 'A' && name[i] <= 'Z'){
+      if(name[i-1] == ' ' || i == 0){
+        putchar(name[i]);
+      }
     }
-    putchar(upper(c));
-  }else if(c >= 'A' && c <= 'Z'){
-    putchar(c);
   }
-
-
 }
+
+
 
 int upper(int c){
 
