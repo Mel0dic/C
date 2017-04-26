e39f334007bb63a23affd6df2fd6b2dae9cffd74
A Mathison <amathisont@gmail.com>
2017-04-26 13:40:31 +0100

    investigation

--- CS50/pset2/Caesar/Caesar.c
+++ CS50/pset2/Caesar/Caesar.c
@@ -21,16 +21,15 @@ int main(int argc, char* argv[]) {
 
 char* caesar(int one){
 
-  // while(one > 26){
-  //   one -= 26;
-  // }
+  //printf("a - 97 = %i + variable = %i %% 26 = %i + 97 = %i\n", 'a' - 97, ('a' - 97) + one, (('a' - 97) + one) % 26, ((('a' - 97) + one) % 26) + 97);
+
   printf("Please Enter A Word or Sentence: \n");
 
   while((c = getchar()) != EOF){
     if(islower(c)){
-      c = (((c - 97)+one)%26)+97;
+      c = (((c - 97)+one)% 26)+97;
     }else if(isupper(c)){
-      c = (((c - 65)+one)%26)+65;
+      c = (((c - 65)+one)% 26)+65;
     }
     putchar(c);
   }
