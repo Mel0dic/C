5b277ffeb796c450cb5ace3aa97fcc0d418ef991
A Mathison <amathisont@gmail.com>
2017-04-26 11:39:24 +0100

    Done

--- CS50/pset2/Caesar/Caesar.c
+++ CS50/pset2/Caesar/Caesar.c
@@ -1,13 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int i;
+char* caesar(int one);
+
+char* word;
+
+int c;
 
 int main(int argc, char* argv[]) {
 
-      printf("%i\n", argc);
+  if(argc == 2){
+    caesar(strtol(argv[1], NULL, 10));
+  }else{
+    printf("Please retry\n");
+  }
 
-      //for(i = 1; i < argc; i++){
-      printf("%s\n", argv[1]);
-      //}
+}
 
+char* caesar(int one){
+
+  while(one > 26){
+    one -= 26;
+  }
+  printf("Please Enter A Word or Sentence: \n");
+
+  while((c = getchar()) != EOF){
+    for(int i = 0; i < one; i++){
+      if(c >= 'a' && c<= 'z'){
+        if(c == 'z'){
+          c = 'a';
+        }else{
+          c++;
+        }
+      }else if(c >= 'A' && c<= 'Z'){
+        if(c == 'Z'){
+          c = 'A';
+        }else{
+          c++;
+        }
+      }
+    }
+    putchar(c);
+  }
 }
