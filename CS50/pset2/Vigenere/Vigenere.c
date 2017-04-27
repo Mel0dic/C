3c1c002a2b0e6cd50362fdc4e05eb68ec88abde8
Sectioned <amathisont@gmail.com>
2017-04-27 12:41:33 +0100

    Test

--- CS50/pset2/Vigenere/Vigenere.c
+++ CS50/pset2/Vigenere/Vigenere.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
-int main(){
+int main(int argc, char* argv[]) {
 
+  if(argc == 2){
+      begin(char* argv[1]);
+  }
 
-  
+}
+
+int begin(char* word){
+
+  printf("%s\n", word);
+
 }
