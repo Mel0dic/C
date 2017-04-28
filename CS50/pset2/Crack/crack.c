9533048726632699d9c27a3b880e4a40a16a9811
A Mathison <amathisont@gmail.com>
2017-04-28 16:44:55 +0100

    progress

--- CS50/pset2/Crack/crack.c
+++ CS50/pset2/Crack/crack.c
@@ -1,16 +1,31 @@
 #include <stdio.h>
+#include <string.h>
 #define _XOPEN_SOURCE
 #include <unistd.h>
 
 char *crypt(const char *key, const char *salt);
+char *decrypt(char* hash, char* salt);
 
-int main(int argc, char* argv[]) {
+char *pass, *hash;
+char salts[2];
+
+int main(int argc, char *argv[]) {
 
   if(argc == 2){
-    printf("%s\n", crypt(argv[1], "Ben"));
+    hash = argv[1];
+    for(int i = 0; i < 2; i++){
+      salts[i] = hash[i];
+    }
+    decrypt(hash, salts);
   }else{
     printf("Usage :./crack hash\n");
     return 1;
   }
+
+}
+
+char *decrypt(char *hashh, char *salt){
+
+  return salt;
 
 }
