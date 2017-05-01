034db2de111d2dee61b5c99085fd7ca130b7fe34
A Mathison <amathisont@gmail.com>
2017-05-01 15:06:26 +0100

    nope

--- CS50/pset2/Crack/crack.c
+++ CS50/pset2/Crack/crack.c
@@ -1,31 +1,36 @@
+#define _XOPEN_SOURCE
+
 #include <stdio.h>
 #include <string.h>
-#define _XOPEN_SOURCE
 #include <unistd.h>
 
 char *crypt(const char *key, const char *salt);
 char *decrypt(char* hash, char* salt);
 
-char *pass, *hash;
-char salts[2];
+char *hash;
+char salts[3];
 
 int main(int argc, char *argv[]) {
 
-  if(argc == 2){
-    hash = argv[1];
-    for(int i = 0; i < 2; i++){
-      salts[i] = hash[i];
-    }
-    decrypt(hash, salts);
-  }else{
+  if(argc != 2){
     printf("Usage :./crack hash\n");
     return 1;
   }
+
+  strncpy(salts, argv[1], 2);
+  salts[2] = '\0';
+
+  decrypt(argv[1], salts);
 
 }
 
 char *decrypt(char *hashh, char *salt){
 
-  return salt;
+  char fullset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  char pass[5] = {'\0', '\0', '\0', '\0', '\0'};
 
+  for(int i = 0; i < sizeof(fullset); i++){
+    printf("%c", fullset[i]);
+  }
+  printf("\n");
 }
