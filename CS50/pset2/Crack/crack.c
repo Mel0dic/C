e21018503c8843314f0612751b7326e62cc827f7
A Mathison <amathisont@gmail.com>
2017-05-02 14:05:31 +0100

    done ish

--- CS50/pset2/Crack/crack.c
+++ CS50/pset2/Crack/crack.c
@@ -5,7 +5,8 @@
 #include <unistd.h>
 
 char *crypt(const char *key, const char *salt);
-void decrypt(char* hash, char* salt);
+int decrypt(char* hash, char* salt);
+void plusplus(char string[], int some);
 
 char *hash;
 char salts[3];
@@ -24,16 +25,39 @@ int main(int argc, char *argv[]) {
 
 }
 
-void decrypt(char *hashh, char *salt){
+int decrypt(char *hashh, char *salt){
 
-  char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  //char alpha[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
   char pass[5] = {'\0', '\0', '\0', '\0', '\0'};
 
-  for(int i = 0; i < sizeof(alpha); i++){
-    printf("%c", alpha[i]);
-  }
-  if(strcmp(hashh, crypt(pass, salt)) == 0){
-      printf("The Password Is: %s", pass);
-  }
+  do{
+    if(strcmp(hashh, crypt(pass, salt)) == 0){
+        printf("The Password Is: %s", pass);
+        printf("\n");
+        return 0;
+    }
+    plusplus(pass, 0);
+  }while(pass[4] == '\0');
+
+  // for(int i = 0; i < sizeof(alpha); i++){
+  //   printf("%c", alpha[i]);
+  // }
+
   printf("\n");
+  return 1;
+}
+
+void plusplus(char string[], int some){
+
+  if(string[some] == '\0'){
+    string[some] = 'a';
+  }else if(string[some] == 'z'){
+    string[some] = 'A';
+  }else if(string[some] == 'Z'){
+    string[some] = 'a';
+    plusplus(string, some+1);
+  }else{
+    string[some]++;
+  }
+
 }
