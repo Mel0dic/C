e7fbe4dbc839feb033599233c95caaf9050bb8db
Ben <amathisont@gmail.com>
2017-10-14 22:08:45 +0100

    Please Core Stop Dumping Me

--- Random/Threading/threadingWork.c
+++ Random/Threading/threadingWork.c
@@ -5,20 +5,23 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <time.h>
+#include <string.h>
 
-
+#define MAXSTRINGLENGTH 10
 
 int decision;
-char* messageTable = [];
-int timeTable = [];
 
+const char * wordList[10] = {"Hello", "hi", "something", "yes", "goodbye", "this", "has", "been", "random", "message"};
 
+void *myThreadFun(void *);
 
-void *myThreadFun(void *vargp);
+char* concatinate(const char *, const char *);
 
 
 
 int main(int argc, char *argv){
+
+	printf("%s\n", wordList[0]);
 
 	pthread_t tid;
 
@@ -37,18 +40,33 @@ int main(int argc, char *argv){
 	}while(decision != 1 && decision != 2);
 
 	if(decision == 1){
+		printf("Generating...\n");
 		srand(time(NULL));
-		for(int messages = 0;messages < (rand() % (10 + 1 - 1) + 1);messages++){
-			char *tempMessage;
+		int messagesMax = (rand() % (10 + 1 - 1) + 1);
+		int timeTable[messagesMax-1];
+		char* messageTable[messagesMax-1];
+		for(int messages = 0;messages < messagesMax;messages++){
+			const char * tempMessage;
+			//Sleep needed to get new time and update srand
+			sleep(1);
 			srand(time(NULL));
 			for(int word; word < (rand() % (7 + 1 - 2) + 2); word++){
-				tempMessage = strcat(tempMessage, wordList[rand() % (10 + 1 - 1) + 1])
+				tempMessage = concatinate(tempMessage, wordList[rand() % (10 + 0 - 1) + 0]);
+				printf("%s\n", tempMessage);
 			}
+			srand(time(NULL));
+			int tempTime = rand() % (10 + 1 - 1) + 1;
 
-			int tempTime = rand() % (10 + 1 - 1) + 1
+			//printf("%s\n", tempMessage);
+			timeTable[messages] = tempTime;
+			messageTable[messages] = tempMessage;
+		}
 
+		for(int i = 0; i < messagesMax; i++){
+			printf("%i, %s\n", timeTable[i], messageTable[i]);
 		}
 	}
+
 
 	return 1;
 
@@ -64,5 +82,16 @@ void *myThreadFun(void *vargp){
 	sleep(sleepTime + 1);
 	printf("Finished Sleeping From Thread %i\n", sleepTime);
 	return NULL;
+
+}
+
+char* concatinate(const char *stringOne, const char *stringTwo){
+
+	char *returnValue = malloc(strlen(stringOne) + strlen(stringTwo) + 1);
+
+	strcpy(returnValue, stringOne);
+	strcpy(returnValue, " ");
+	strcpy(returnValue, stringTwo);
+	return returnValue;
 
 }
